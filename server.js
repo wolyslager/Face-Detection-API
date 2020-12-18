f501ffcb9e5f0b39a58b92e6f2e909ea924e6f71
wolyslager <olyslager.willy@gmail.com>
2020-12-18 13:53:14 -0600

    adding database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-contoured-02131',
     user : 'williamolyslager',
     password : '',
     database : 'smart-brain'
